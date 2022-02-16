1c100a11fc740eded4bbce25db54bd55c4c2424e
108062138 <poyuwu17@gmail.com>
2022-02-16 17:07:43 +0800

    correct the port

--- binaryToGrayTB.v
+++ binaryToGrayTB.v
@@ -4,16 +4,15 @@ module TB;
 reg [3:0] cnt;
 wire [3:0] res;
 binaryToGray BINARYTOGRAY(
-.a(cnt[0]),
-.b(cnt[1]),
-.c(cnt[2]),
-.d(cnt[3]),
-.w(res[0]),
-.x(res[1]),
-.y(res[2]),
-.z(res[3])
-)
-
+.a(cnt[3]),
+.b(cnt[2]),
+.c(cnt[1]),
+.d(cnt[0]),
+.w(res[3]),
+.x(res[2]),
+.y(res[1]),
+.z(res[0])
+;
 initial begin
     for (cnt = 4'b0000; cnt <=4'b1111; cnt = cnt + 4'b0001) begin
         #5
