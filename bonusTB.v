4ac4c28976afb7844c211640c007169785b251a3
108062138 <poyuwu17@gmail.com>
2022-02-16 18:37:36 +0800

    add tb

--- bonusTB.v
+++ bonusTB.v
@@ -7,21 +7,18 @@ reg operator;
 wire [3:0] res;
 wire overflow;
 
-ALU4bit A(
+signCmp A(
 .a(a),
 .b(b),
-.operator(operator),
 .s(res),
-.overflow(overflow)
 );
 
 initial begin
     operator = 0;
+    b = 4'b1001;
     for (a = 4'b0000; a <4'b1111; a = a + 4'b0001) begin
-        for (b = 4'b0000; b < 4'b1111; b = b + 4'b0001)begin
-           #5
-            $display("current loop=%d %d", a,b); 
-        end
+       #5
+        $display("current loop=%d %d", a,b); 
     end
 end
 
