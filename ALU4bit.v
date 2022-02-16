39671d86693fd6ceecc299e9bf297b061b5503f8
108062138 <poyuwu17@gmail.com>
2022-02-16 17:35:51 +0800

    add ALU4bit

---
+++ ALU4bit.v
@@ -0,0 +1,35 @@
+`timescale 1ns/1ps
+
+module ALU4bit (
+input [3:0] a,
+input [3:0] b,
+input operator,
+output [3:0]s,
+output overflow
+);
+wire [4:0] C;
+FullAdder FA0(.x(a[0]), .y(operator ^ b[0]),.cin(voperator), .A(s[0]) ,.cout(C[1]));
+FullAdder FA1(.x(a[1]), .y(operator ^ b[1]),.cin(     C[1]), .A(s[1]) ,.cout(C[2]));
+FullAdder FA2(.x(a[2]), .y(operator ^ b[2]),.cin(     C[2]), .A(s[2]) ,.cout(C[3]));
+FullAdder FA3(.x(a[3]), .y(operator ^ b[3]),.cin(     C[3]), .A(s[3]) ,.cout(C[4]));
+
+assign overflow = C[4] ^ C[3];
+
+endmodule
+
+module FullAdder (
+input wire x,
+input wire y,
+input wire cin,
+output wire A,
+output wire cout
+);
+
+wire p,r,s;
+assign p = x ^ y;
+assign r = cin & p;
+assign s = x & y;
+assign cout = r | s;
+assign A = cin ^ p;
+    
+endmodule
---
+++ ALU4bitTB.v
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+
+module TB;
+reg [3:0] a;
+reg [3:0] b;
+reg operator;
+wire [3:0] res;
+wire overflow;
+
+ALU4bit ALUTB(
+.a(a),
+.b(b),
+.operator(operator),
+.s(res),
+.overflow(overflow)
+);
+
+initial begin
+    operator = 0;
+    b = 4'b0011;
+    for (a = 4'b0000; a <4'b1111; a = a + 4'b0001) begin
+        #5
+        $display("current loop=%d", a);
+    end
+end
+
+endmodule
